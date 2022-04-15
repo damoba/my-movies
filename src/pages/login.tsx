26c0cc0448e6f2033f722d1ae7db4bc97f7a91ea
David Moreno-Bautista <davmoba4@gmail.com>
2022-04-15 19:18:14 -0400

    feat: set up login background and logo

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,25 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import {useRouter} from 'next/router'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-import styles from '../styles/Login.module.css'
+import styles from "../styles/Login.module.css";
 
 const LoginPage: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>My Movies - log in or sign up</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Hello world
-    </div>
-  )
-}
+      <div className={styles.page}>
+        <div className={styles.navbar}>
+          <span className={styles.logo}>My Movies</span>
+          <div className={styles.gradient}></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
