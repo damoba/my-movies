57d1d32b3a704165997c8b3f131418bd21b80df4
David Moreno-Bautista <davmoba4@gmail.com>
2022-04-19 22:48:08 -0400

    chore: cleaned up code

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -12,8 +12,9 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const firebaseApp =
-  getApps().length == 0 ? initializeApp(firebaseConfig) : getApp();
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
 const auth = getAuth(firebaseApp);
 const db = getDatabase(firebaseApp);
 
