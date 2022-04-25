ef4ee62a8aa810db5e97725a722d3149a2f0c329
David Moreno-Bautista <davmoba4@gmail.com>
2022-04-24 20:19:24 -0400

    feat: added profile icon for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import styles from "./Header.module.css";
 
 import React, { FunctionComponent, useRef, useState } from "react";
-import { Search } from "@material-ui/icons";
+import { ArrowDropDown, Search } from "@material-ui/icons";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 const Header: FunctionComponent = () => {
   const router = useRouter();
@@ -36,24 +37,48 @@ const Header: FunctionComponent = () => {
         isScrolled ? `${styles.header} ${styles.scrolled}` : styles.header
       }
     >
-      <div className={styles.container}>
+      <div className={styles.navMainContainer}>
         <span className={styles.logo}>My Movies</span>
-        <div
-          className={
-            searchIsOpen ? `${styles.search} ${styles.open}` : styles.search
-          }
-          onClick={handleSearchClick}
-        >
-          <Search className={styles.searchIcon} style={{ fontSize: 20 }} />
-          <form onSubmit={() => router.push(`/search?query=${encodeURIComponent(searchRef.current.value)}`)}>
-            <input
-              ref={searchRef}
-              type="search"
-              onBlur={() => setSearchIsOpen(false)}
-              placeholder="Search..."
-            />
-            <button type="submit" style={{display: "none"}}/>
-          </form>
+        <div className={styles.navRightContainer}>
+          <div
+            className={
+              searchIsOpen ? `${styles.search} ${styles.open}` : styles.search
+            }
+            onClick={handleSearchClick}
+          >
+            <Search className={styles.searchIcon} style={{ fontSize: 20 }} />
+            <form
+              onSubmit={() =>
+                router.push(
+                  `/search?query=${encodeURIComponent(searchRef.current.value)}`
+                )
+              }
+            >
+              <input
+                ref={searchRef}
+                type="search"
+                onBlur={() => setSearchIsOpen(false)}
+                placeholder="Search..."
+              />
+              <button type="submit" style={{ display: "none" }} />
+            </form>
+          </div>
+          <div className={styles.profile}>
+            <div className={styles.profilePicContainer}>
+              <Image
+                src="/images/profile_picture.png"
+                height="25px"
+                width="25px"
+                alt="Profile Picture"
+                objectFit="cover"
+              />
+            </div>
+            <ArrowDropDown/>
+            <div className={styles.options}>
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </header>
