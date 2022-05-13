15a1e3da789a7d82808370b776c162ae550cc089
David Moreno-Bautista <davmoba4@gmail.com>
2022-05-13 18:10:21 -0400

    chore: removed unused code

--- src/firebase/api.ts
+++ src/firebase/api.ts
@@ -1,6 +1,6 @@
-import { MovieFromFeatured, MovieFromThumbnail } from "../../typings";
+import { MovieFromFeatured } from "../../typings";
 import { db } from "./config";
-import { child, get, ref, remove, set } from "firebase/database";
+import { ref, remove, set } from "firebase/database";
 
 /**
  * Adds a movie from the featured display to the collection database.
@@ -36,24 +36,4 @@ const removeMovie = (uid: string, id: number) => {
   remove(ref(db, `movies/${uid}/${id}`));
 };
 
-/**
- * Returns the collection of movies for the current user.
- * @param {string} uid UID of user to get movie collection of
- * @returns {MovieFromThumbnail[]} The collection of movies
- */
-const getMovies = (uid: string): MovieFromThumbnail[] | void => {
-  const dbRef = ref(db);
-  get(child(dbRef, `movies/${uid}`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      const moviesFromThumbnail = [];
-      const items = snapshot.val();
-      const keys = Object.keys(items);
-      for (let i = 0; i < keys.length; i++) {
-        moviesFromThumbnail.push(items[keys[i]]);
-      }
-      return moviesFromThumbnail;
-    }
-  });
-};
-
-export { addMovie, removeMovie, getMovies };
+export { addMovie, removeMovie };
